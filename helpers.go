08b2491b3de70b885a43a3fe22eef8de7105310e
k1 <keyvan.soltan18@gmail.com>
2024-11-19 00:08:26 +0330

    move helpers funcs

---
+++ helpers.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+
+
+
+func checkIfError(err error) {
+	if err == nil {
+		return
+	}
+
+	fmt.Printf("\x1b[31;1m%s\x1b[0m\n", fmt.Sprintf("error: %s", err))
+	os.Exit(1)
+}
